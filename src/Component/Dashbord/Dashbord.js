5ab888ee132110a25850e8dc7559bec0a35cf347
Niloy-Dey <niloydeyce@gmail.com>
2022-04-05 01:26:33 +0600

    rchart install finished

--- src/Component/Dashbord/Dashbord.js
+++ src/Component/Dashbord/Dashbord.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from 'recharts';
 
-const Dashbord = () => {
+const Dashbord = () => { 
+        const [datas, setDatas] = useState();
+        useEffect(()=> {
+            fetch('../../../data.json')
+            .then(res => res.json())
+            .then(data => console.log(data));
+        },[])
     return (
         <div>
-            <h1>this is dashbord </h1>
+            <div>
+                {/* <BarChart width={730} height={250} data={data}> */}
+                <BarChart width={730} height={250}> 
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="pv" fill="#8884d8" />
+                <Bar dataKey="uv" fill="#82ca9d" />
+                </BarChart>
+            </div>
         </div>
     );
 };
