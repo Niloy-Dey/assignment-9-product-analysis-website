ddc767e2c8109a1fdcec0aac7723dd08f843161c
Niloy-Dey <niloydeyce@gmail.com>
2022-04-05 09:53:45 +0600

    dashbord work is complete

--- src/Component/Dashbord/Dashbord.js
+++ src/Component/Dashbord/Dashbord.js
@@ -1,26 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, CartesianGrid, Legend, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
 
-const Dashbord = () => { 
-        const [datas, setDatas] = useState();
-        useEffect(()=> {
-            fetch('../../../data.json')
+const Dashbord = () => {
+    const [data, setData] = useState();
+    useEffect(() => {
+        fetch('data.json')
             .then(res => res.json())
-            .then(data => console.log(data));
-        },[])
+            .then(data => setData(data));
+    }, [])
     return (
-        <div>
+        <div className='flex justify-between m-20'>
             <div>
-                {/* <BarChart width={730} height={250} data={data}> */}
-                <BarChart width={730} height={250}> 
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="pv" fill="#8884d8" />
-                <Bar dataKey="uv" fill="#82ca9d" />
+                <BarChart width={730} height={250} data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis dataKey='investment'/>
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="month" fill="#8884d8" />
+                    <Bar dataKey="investment" fill="#82ca9d" />
                 </BarChart>
+                <h1 className='mt-10 text-center font-bold text-green-500 text-2xl '> <u>Bar chart</u></h1>
+            </div>
+            <div>
+                <PieChart width={750} height={300} data={data}>
+                    <Pie data={data} dataKey="investment" nameKey="month" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+                    <Pie data={data} dataKey="investment" nameKey="month" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+                    <Tooltip />
+                </PieChart>
+                <h1 className='text-center font-bold text-green-500 text-2xl '> <u>Pie chart</u></h1>
             </div>
         </div>
     );
